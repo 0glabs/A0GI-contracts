b46426549aa7d00eb3db016f286a1e2e397ae122
MiniFrenchBread <103425574+MiniFrenchBread@users.noreply.github.com>
2025-01-09 15:23:19 +0800

    feat: update interface

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,33 +1,36 @@
+import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-abi-exporter";
 import { HardhatUserConfig } from "hardhat/types";
 
+import "./src/tasks/tx";
+
 const config: HardhatUserConfig = {
-    paths: {
-        artifacts: "build/artifacts",
-        cache: "build/cache",
-        sources: "contracts",
-    },
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.20",
-                settings: {
-                    evmVersion: "istanbul",
-                    optimizer: {
-                        enabled: true,
-                        runs: 200,
-                    },
-                },
-            },
-        ],
-    },
-    abiExporter: {
-        path: "./abis",
-        runOnCompile: true,
-        clear: true,
-        flat: true,
-        format: "json",
-    },
+  paths: {
+    artifacts: "build/artifacts",
+    cache: "build/cache",
+    sources: "contracts",
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          evmVersion: "istanbul",
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  abiExporter: {
+    path: "./abis",
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    format: "json",
+  },
 };
 
 export default config;
